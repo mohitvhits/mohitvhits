fb360d95f0db61dc3a9f1b4e603b99ed91745792
mohitvhits <mohitvamja.vhits@gmail.com>
2023-10-09 12:00:36 +0530

    upload test.php file

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Web30india
+ * Copyright (C) 2023 Web30india <hello@web30india.com>
+ *
+ * @category Web30india
+ * @package Web30india_Faq
+ * @copyright Copyright (c) 2023 Web30india (https://web30india.com/)
+ * @license http://opensource.org/licenses/gpl-3.0.html GNU General Public License,version 3 (GPL-3.0)
+ * @author Web30india (https://web30india.com/)
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Web30india_Faq',
+    __DIR__
+);
