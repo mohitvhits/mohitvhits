7986321a24d7ae5d9e5491a073e959cda5684297
mohitvhits <mohitvamja.vhits@gmail.com>
2023-10-10 10:48:33 +0530

    add file

--- test.php
+++ test.php
@@ -11,6 +11,6 @@
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Web30india_Faq_test',
+    'Web30india_Faq_test_magento',
     __DIR__
 );
